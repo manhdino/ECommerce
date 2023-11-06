d766eed52402093f3164d0916d3b1a0cb3dd6f09
manhdino <manhnguyen1238@gmail.com>
2023-11-06 15:02:57 +0700

    add features services home page

---
+++ src/components/CollectionComponent/CollectionComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import '../../css/CollectionComponent.css';
+function CollectionComponent() {
+    return (
+
+        <div>CollectionComponent</div>
+
+    )
+}
+
+export default CollectionComponent
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,18 @@
 import React from 'react'
-import '../../css/HomePage.css';
 import SliderCompoent from '../../components/SliderComponent/SliderCompoent';
 import ImgSlider1 from '../../images/slider_1.jpg';
 import ImgSlider2 from '../../images/slider_2.jpg';
 import ImgSlider3 from '../../images/slider_4.jpg';
+import CollectionComponent from '../../components/CollectionComponent/CollectionComponent';
+import FeaturesComponent from '../../components/Features/FeaturesComponent';
+
 function HomePage() {
 
     return (
         <>
-            {/* <section id="slider">
-                <h2>SUPER VALUE DEALS</h2>
-                <h4>On all products</h4>
-                <p>Save more with coupons & up to 70% off!</p>
-                <NavLink className='shop' to='/shop'>Shop Now</NavLink>
-            </section> */}
             <SliderCompoent arrImages={[ImgSlider1, ImgSlider2, ImgSlider3]} />
+            <CollectionComponent />
+            <FeaturesComponent />
         </>
     )
 }
