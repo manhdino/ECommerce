ae1ace10e6b3dd95d3bdcc0973cc4f5b15ce62c3
manhdino <manhnguyen1238@gmail.com>
2023-11-07 18:49:28 +0700

    add responsive features services

--- src/components/FeaturesComponent/FeaturesComponent.js
+++ src/components/FeaturesComponent/FeaturesComponent.js
@@ -6,26 +6,28 @@ import img_contact from '../../images/contact.png';
 import '../../css/FeaturesComponent.css';
 function FeaturesComponent() {
     return (
-        <div class="features">
-            <div class="feature">
-                <img src={img_shipping} alt="" class="featureIcon" />
-                <span class="featureTitle">FREE SHIPPING</span>
-                <span class="featureDesc">Free worldwide shipping on all orders.</span>
-            </div>
-            <div class="feature">
-                <img class="featureIcon" src={img_return} alt="" />
-                <span class="featureTitle">30 DAYS RETURN</span>
-                <span class="featureDesc">No question return and easy refund in 14 days.</span>
-            </div>
-            <div class="feature">
-                <img class="featureIcon" src={img_gift} alt="" />
-                <span class="featureTitle">GIFT CARDS</span>
-                <span class="featureDesc">Buy gift cards and use coupon codes easily.</span>
-            </div>
-            <div class="feature">
-                <img class="featureIcon" src={img_contact} alt="" />
-                <span class="featureTitle">CONTACT US!</span>
-                <span class="featureDesc">Keep in touch via email and support system.</span>
+        <div class='features-container'>
+            <div class="features">
+                <div class="feature">
+                    <img src={img_shipping} alt="" class="featureIcon" />
+                    <span class="featureTitle">FREE SHIPPING</span>
+                    <span class="featureDesc">Free worldwide shipping on all orders.</span>
+                </div>
+                <div class="feature">
+                    <img class="featureIcon" src={img_return} alt="" />
+                    <span class="featureTitle">30 DAYS RETURN</span>
+                    <span class="featureDesc">No question return and easy refund in 14 days.</span>
+                </div>
+                <div class="feature">
+                    <img class="featureIcon" src={img_gift} alt="" />
+                    <span class="featureTitle">GIFT CARDS</span>
+                    <span class="featureDesc">Buy gift cards and use coupon codes easily.</span>
+                </div>
+                <div class="feature">
+                    <img class="featureIcon" src={img_contact} alt="" />
+                    <span class="featureTitle">CONTACT US!</span>
+                    <span class="featureDesc">Keep in touch via email and support system.</span>
+                </div>
             </div>
         </div>
     )
--- src/components/HeaderComponent/HeaderComponent.js
+++ src/components/HeaderComponent/HeaderComponent.js
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../images/logo.png'
 import '../../css/HeaderComponent.css';
 import { NavLink } from 'react-router-dom';
 function HeaderComponent({ children }) {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const openMenu = () => {
+        setMenuOpen(true);
+    };
+
+    const closeMenu = () => {
+        setMenuOpen(false);
+    }
     return (
         <>
             <section id="header">
+
                 <a href="/"><img src={logo} class="logo" alt="" /></a>
-                <div>
-                    <ul id="navbar" class="center">
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/'>Home</NavLink></li>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/shop'>Shop</NavLink></li>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/blog'>Blog</NavLink></li>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/services'>Services</NavLink></li>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/contact'>Contact</NavLink></li>
-                        <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/cart'><i class="fa-solid fa-cart-shopping"></i></NavLink></li>
-                    </ul>
+                <div id="mobile">
+                    <NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/cart'><i class="fa-solid fa-cart-shopping"></i></NavLink>
+                    <i id="bar" class="fa-solid fa-bars" onClick={openMenu}></i>
                 </div>
+
+
+                <ul id="navbar" class={menuOpen ? "open center" : "close center"}>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/'>Home</NavLink></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/shop'>Shop</NavLink></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/blog'>Blog</NavLink></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/services'>Services</NavLink></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/contact'>Contact</NavLink></li>
+                    <li><NavLink className={({ isActive }) => (isActive ? 'active nav' : 'nav')} to='/cart'><i id="cart" class="fa-solid fa-cart-shopping"></i></NavLink> </li>
+                    <i id="close" class="fa-solid fa-xmark" onClick={closeMenu}></i>
+                </ul>
+
             </section>
             {children}
         </>
