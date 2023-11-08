3d1ce0c8a5bf000f6d23ee13248d316b96288697
manhdino <manhnguyen1238@gmail.com>
2023-11-08 15:27:40 +0700

    add hero banner and responsive, delete slider component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,17 +1,14 @@
 import React from 'react'
-import SliderCompoent from '../../components/SliderComponent/SliderCompoent';
-import ImgSlider1 from '../../images/slider_1.jpg';
-import ImgSlider2 from '../../images/slider_2.jpg';
-import ImgSlider3 from '../../images/slider_4.jpg';
 import CollectionComponent from '../../components/CollectionComponent/CollectionComponent';
 import FeaturesComponent from '../../components/FeaturesComponent/FeaturesComponent';
 import DiscoverComponent from '../../components/DiscoverComponent/DiscoverComponent';
+import HeroComponent from '../../components/HeroComponent/HeroComponent';
 
 function HomePage() {
 
     return (
         <>
-            {/* <SliderCompoent arrImages={[ImgSlider1, ImgSlider2, ImgSlider3]} /> */}
+            <HeroComponent />
             <CollectionComponent />
             <FeaturesComponent />
             <DiscoverComponent />
