ba5fef4acc6893a17b109f2dc9663f27aecb3499
manhdino <manhnguyen1238@gmail.com>
2023-12-08 03:58:47 +0700

    update .env

--- Server/app.js
+++ Server/app.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const auth = require("./modules/auth/auth.route");
 const product = require("./modules/product/product.route");
 const user = require("./modules/user/user.route");
-const cart = require("./modules/cart/cart.route")
-const order = require("./modules/order/order.route")
+const cart = require("./modules/cart/cart.route");
+const order = require("./modules/order/order.route");
 
 const app = express();
 
 app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: false,
-    })
+  express.urlencoded({
+    extended: false,
+  })
 );
 
 app.use("/auth", auth);
@@ -24,17 +24,18 @@ app.use("/cart", cart);
 app.use("/order", order);
 
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI_DINOMANH;
 
-mongoose.connect(MONGODB_URI)
-    .then(() => {
-        console.log('Kết nối đến MongoDB thành công');
-    })
-    .catch((err) => {
-        console.error('Kết nối đến MongoDB thất bại', err);
-    });
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Kết nối đến MongoDB thành công");
+  })
+  .catch((err) => {
+    console.error("Kết nối đến MongoDB thất bại", err);
+  });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 module.exports = app;
